9b5649cadb5518776a317ed558bf9d7df419d98c
Glebeeezy <daf701@mail.ru>
2023-09-16 13:47:06 +0300

    calc dev in process

--- MyCalculatorHW/Program.cs
+++ MyCalculatorHW/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите название калькулятора");
+
+            string name = Console.ReadLine();
+
+            DoubleCalculator calculator = new DoubleCalculator(name);
+
+            bool controller = true;
+            while (controller)
+            {
+                calculator.Calculate();
+                controller =Controller();
+            }
+
+            bool Controller()
+            {
+                Console.WriteLine("Введите 1 если хотите продолжить вычисления\nВведите 2 если хотите прекратить");
+
+                int number = int.Parse(Console.ReadLine());
+
+                if (number == 1)
+                {
+                    return true;
+                }
+
+                else if(number == 2)
+                {
+                    return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     }
 }
