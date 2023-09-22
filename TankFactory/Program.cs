db49e003e2ddb7eeeaff6ad2ab2beda7196a2a97
Glebeeezy <daf701@mail.ru>
2023-09-22 14:53:43 +0300

    empty solution created

---
+++ TankFactory/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TankFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
